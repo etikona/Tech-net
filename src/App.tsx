d1caa5e39d123c432eab45dfd4b3816620e916bc
eti kona <etikonapal@gmail.com>
2024-01-20 16:22:19 +0600

    In app, increament and decrement design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>Hello World. Hi from Tech-net</h1>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center mt-40">
+      <button className="btn border-2 border-green-500 text-xl font-bold p-2">
+        Increment
+      </button>
+      <div>0</div>
+      <button className="btn border-2 border-orange-500 text-xl font-bold p-2">
+        Decrement
+      </button>
+    </div>
+  );
+};
 
 export default App;
