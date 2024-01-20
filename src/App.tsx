5021f1bd80031a4a9b910900d465a1c496926841
eti kona <etikonapal@gmail.com>
2024-01-20 17:13:23 +0600

    Implent functionality by using of useDispatch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 const App = () => {
   const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="text-center mt-40">
-      <button className="btn border-2 border-green-500 text-xl font-bold p-2">
+      <button
+        className="btn border-2 border-green-500 text-xl font-bold p-2"
+        onClick={() => dispatch(increment())}
+      >
         Increment
       </button>
       <div>{count}</div>
-      <button className="btn border-2 border-orange-500 text-xl font-bold p-2">
+      <button
+        className="btn border-2 border-orange-500 text-xl font-bold p-2"
+        onClick={() => dispatch(decrement())}
+      >
         Decrement
       </button>
     </div>
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -16,4 +16,5 @@ const counterSlice = createSlice({
   },
 });
 
+export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
