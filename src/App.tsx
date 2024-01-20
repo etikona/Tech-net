bc3ca46e347be43c9b964aaa5feaa63540187a70
eti kona <etikonapal@gmail.com>
2024-01-20 19:52:31 +0600

    Middleware added in store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {} from "react-redux";
 import { RootState } from "./redux/store";
-import { decrement, increment } from "./redux/features/counter/counterSlice";
+import {
+  decrement,
+  decrementByAmount,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 const App = () => {
-  const { count } = useSelector((state: RootState) => state.counter);
-  const dispatch = useDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <div className="text-center mt-40">
       <button
@@ -14,12 +20,24 @@ const App = () => {
       >
         Increment
       </button>
+      <button
+        className="btn border-2 border-green-500 text-xl font-bold p-2 ml-2 "
+        onClick={() => dispatch(incrementByAmount(10))}
+      >
+        increment Value
+      </button>
       <div>{count}</div>
       <button
         className="btn border-2 border-orange-500 text-xl font-bold p-2"
         onClick={() => dispatch(decrement())}
       >
         Decrement
+      </button>
+      <button
+        className="btn border-2 border-orange-500 text-xl font-bold p-2 ml-2 "
+        onClick={() => dispatch(decrementByAmount(10))}
+      >
+        Decrement Value
       </button>
     </div>
   );
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,8 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+// Interface
+interface countState {
+  count: number;
+}
+// Declare Initial State
+const initialState: countState = {
   count: 0,
 };
+
+// Counter Slice
 const counterSlice = createSlice({
   name: "counter",
   initialState,
@@ -10,11 +18,18 @@ const counterSlice = createSlice({
     increment: (state) => {
       state.count = state.count + 1;
     },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
     decrement: (state) => {
       state.count -= 1;
+    },
+    decrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count -= action.payload;
     },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, decrementByAmount } =
+  counterSlice.actions;
 export default counterSlice.reducer;
