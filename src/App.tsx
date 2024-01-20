3b44e89566886082b0d0ef1fd3415ae4e61729a8
eti kona <etikonapal@gmail.com>
2024-01-20 17:04:52 +0600

    Redux reducer added to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 const App = () => {
+  const { count } = useSelector((state: RootState) => state.counter);
   return (
     <div className="text-center mt-40">
       <button className="btn border-2 border-green-500 text-xl font-bold p-2">
         Increment
       </button>
-      <div>0</div>
+      <div>{count}</div>
       <button className="btn border-2 border-orange-500 text-xl font-bold p-2">
         Decrement
       </button>
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -3,15 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   count: 0,
 };
-export const counterSlice = createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
     increment: (state) => {
-      state.count += 1;
+      state.count = state.count + 1;
     },
     decrement: (state) => {
       state.count -= 1;
     },
   },
 });
+
+export default counterSlice.reducer;
